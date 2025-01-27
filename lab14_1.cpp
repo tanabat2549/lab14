160c6f1b0ca3679b703e279f9dff920f5cd4e106
Thanabat <tanabat2549kuayjeng@gmail.com>
2025-01-27 22:20:25 +0700

    cp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 using namespace std;
 
+
 template <typename T>
 void insertionSort(T d[],int N){
-
+    for(int i = 0;i < N -1;i++){
+        cout << "Pass " << i+1 << ":";
+        for(int j = i + 1;j > 0;j--){
+            if(d[j] > d[j - 1]){
+                T tmp = d[j];
+                d[j] = d[j - 1];
+                d[j - 1] = tmp;
+            }
+        }
+        for(int k = 0;k < N;k++){
+            cout << d[k] << " ";
+        }
+        cout << "\n";
+    }
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
